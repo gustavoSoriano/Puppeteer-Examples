f813be8480b15e4504a85d2464d430346bcd6706
Gustavo Soriano <sorianox2010@hotmail.com>
2021-11-16 15:52:14 -0300

    Update app.js

--- screenshot/app.js
+++ screenshot/app.js
@@ -2,7 +2,7 @@ const puppeteer   = require("puppeteer")
 
 const Run = async () => {
   try{
-    const navegador = await puppeteer.launch({args: ['--no-sandbox'], headless: true})
+    const navegador = await puppeteer.launch({args: ['--no-sandbox', '--disable-notifications'], headless: true})
     const pagina    = await navegador.newPage() 
     await pagina.goto(`https://github.com`)
     await pagina.screenshot({path:'./github.png'})
